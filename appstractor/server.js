cb608baccba7642f73bb17c39af22ade636def7b
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2019-11-15 12:58:42 -0500

    ability to save multiple images implemented

--- appstractor/server.js
+++ appstractor/server.js
@@ -10,43 +10,56 @@ app.use(express.static('public'));
 
 app.use(methodOverride('_method'));
 
+//required parameters for exporting canvas body document as string 
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json({limit: '50mb', extended: true}));
 
 //GET
+//Index
 app.get('/appstractor', (req,res) => {
     res.render('index.ejs');
 });
 
+//Create
 app.get('/appstractor/render', (req,res) => {
     res.render('render.ejs');
 });
 
+//Show
 app.get('/appstractor/gallery', (req,res) => {
+    res.render('gallery.ejs',{data:data });  
+});
+
+//Iframe within Create
+app.get('/appstractor/blank_canvas', (req,res) => {
+    res.render('blank_canvas.ejs', {data: data});
+});
+
+//Iframe within Show, only if user browser does not support capture-chrome npm 
+app.get('/appstractor/saved_canvas/:index', (req,res) => {
+    res.render('saved.ejs',{data: data[req.params.index]});
+}); 
+//PUT
+app.put('/appstractor/render/new', (req, res) => {
+    //creates document
+    const document = {
+        id: data.length,
+        dom: req.body.dom,
+        img: null
+    }
+    data.push(document);
+    //once document is created, its dom property can be passed to /saved/:index to be rendered, then captured 
     capture({
-        url: 'http://localhost:3000/appstractor/saved/',
+        url: `http://localhost:3000/appstractor/saved_canvas/${data.length - 1}`,
         width: 3600,
         height: 2400,
         }).then(screenshot => {
-        fs.writeFileSync(`${__dirname}/public/saved/example.png`, screenshot)
-        console.log('open example.png')
+        fs.writeFileSync(`${__dirname}/public/saved/appstractor${data.length - 1}.png`, screenshot)
+        console.log('image saved')
     });
-    res.render('gallery.ejs',{data:data});
-
-    // res.render('gallery.ejs',{data:data});        
-});
-
-app.get('/appstractor/canvas', (req,res) => {
-    res.render('canvas.ejs');
-});
-
-app.get('/appstractor/saved', (req,res) => {
-    res.render('saved.ejs',{data: data[0]});
-}); 
-//PUT
-app.put('/appstractor/gallery/new', (req, res) => {
-    data.push(req.body.html);
-    console.log(data);
+    //the newly created image path then gets stored in the document 
+    data[data.length - 1].img = `${__dirname}/public/saved/appstractor${data.length - 1}.png`; 
+    res.status(204).send();
 });
 
 //Listen
