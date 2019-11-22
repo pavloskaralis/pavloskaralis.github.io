f9e9e7b3febe9f1eb8765f48a399bec230726f4a
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2019-11-22 13:10:45 -0500

    fixing glitch

---
+++ appstractor/controllers/appstractor.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router();
+const capture = require('capture-chrome');
+const fs = require('fs');
+const Appstraction = require('../models/appstraction.js');
+//test variable 
+const currentUser = {username: 'Username', email: 'first.last@gmail.com', password: '12345'}
+
+//GET
+//Index
+router.get('/', (req,res) => {
+    res.render('index.ejs');
+});
+
+//Create
+router.get('/render', (req,res) => {
+    res.render('new.ejs');
+});
+
+//Iframe within Create
+router.get('/blank_canvas', (req,res) => {
+    //server identifies current user to attach their username to their content
+    res.render('blank_canvas.ejs',  {user: currentUser.username});
+});
+
+//Show + Edit 
+router.get('/gallery', (req,res) => {
+    Appstraction.find({user: currentUser.username}, (err, data) => {
+        res.render('show.ejs',{data: data, user: currentUser.username});
+    });
+});
+
+//Iframe within Show + Edit
+router.get('/saved_canvas/:id', (req,res) => {
+    Appstraction.findById(req.params.id, (err, doc) => {
+        res.render('saved_canvas.ejs',{doc: doc});
+    });
+}); 
+
+
+//POST: Save
+router.post('/blank_canvas', (req, res) => {
+    Appstraction.create(req.body, (err, doc) => {
+        res.status(204).send();
+    });
+});
+
+//POST: Download
+//Needs to be done with id and not index to avoid capture-chrome cache error 
+router.post('/saved_canvas/:id', (req, res) => {
+        capture({
+            url: `http://localhost:3000/appstractor/saved_canvas/${req.params.id}`,
+            width: 3600,
+            height: 2400,
+            }).then(screenshot => {
+            fs.writeFileSync(`${__dirname + '/../'}/public/saved/appstractorUsername.png`, screenshot);
+        });
+        res.status(204).send();
+});
+
+//PUT
+router.put('/saved_canvas/:id', (req,res) => {
+    Appstraction.findByIdAndUpdate(req.params.id,{$set: {dom: req.body.dom}}, {new: true}, (err, doc) => {
+        res.status(204).send();
+    });
+});
+
+//Delete
+router.delete('/saved_canvas/:id', (req, res) => {
+    Appstraction.findByIdAndDelete(req.params.id, (err, doc) => {
+        res.status(204).send();
+    });
+});
+
+module.exports = router; 
+
+
--- appstractor/server.js
+++ appstractor/server.js
@@ -2,16 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const methodOverride = require('method-override');
-const capture = require('capture-chrome');
-const fs = require('fs');
+const appstractorController = require('./controllers/appstractor.js');
 
 const mongoose = require('mongoose');
 const mongoURI = 'mongodb://localhost:27017/' + 'appstraction';
-const Appstraction = require('./models/appstraction.js');
-
-//test variable 
-const currentUser = {username: 'Username', email: 'first.last@gmail.com', password: '12345'}
-// const data = [];
 
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
@@ -20,72 +14,7 @@ app.use(methodOverride('_method'));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json({limit: '50mb', extended: true}));
 
-//GET
-//Index
-app.get('/appstractor', (req,res) => {
-    res.render('index.ejs');
-});
-
-//Create
-app.get('/appstractor/render', (req,res) => {
-    res.render('new.ejs');
-});
-
-//Iframe within Create
-app.get('/appstractor/blank_canvas', (req,res) => {
-    //server identifies current user to attach their username to their content
-    res.render('blank_canvas.ejs',  {user: currentUser.username});
-});
-
-//Show + Edit 
-app.get('/appstractor/gallery', (req,res) => {
-    Appstraction.find({user: currentUser.username}, (err, data) => {
-        res.render('show.ejs',{data: data});
-    });
-});
-
-//Iframe within Show + Edit
-app.get('/appstractor/saved_canvas/:id', (req,res) => {
-    Appstraction.findById(req.params.id, (err, doc) => {
-        res.render('saved_canvas.ejs',{doc: doc});
-    });
-}); 
-
-
-//POST: Save
-app.post('/appstractor/blank_canvas', (req, res) => {
-    Appstraction.create(req.body, (err, doc) => {
-        res.status(204).send();
-    });
-});
-
-//POST: Download
-//Needs to be done with id and not index to avoid capture-chrome cache error 
-app.post('/appstractor/saved_canvas/:id', (req, res) => {
-    capture({
-        url: `http://localhost:3000/appstractor/saved_canvas/${req.params.id}`,
-        width: 3600,
-        height: 2400,
-        }).then(screenshot => {
-        fs.writeFileSync(`${__dirname}/public/saved/appstractor.png`, screenshot);
-        console.log('image saved');
-    });
-    res.status(204).send();
-});
-
-//PUT
-app.put('/appstractor/saved_canvas/:id', (req,res) => {
-    Appstraction.findByIdAndUpdate(req.params.id,{$set: {dom: req.body.dom}}, {new: true}, (err, doc) => {
-        res.status(204).send();
-    });
-});
-
-//Delete
-app.delete('/appstractor/saved_canvas/:id', (req, res) => {
-    Appstraction.findByIdAndDelete(req.params.id, (err, doc) => {
-        res.status(204).send();
-    });
-});
+app.use('/appstractor', appstractorController);
 
 //Mongoose
 mongoose.connect(mongoURI);
