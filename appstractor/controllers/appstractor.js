9fc759112ca0cbf9b000ef8656e4a4fe59372b02
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2019-11-22 23:26:28 -0500

    test

--- appstractor/controllers/appstractor.js
+++ appstractor/controllers/appstractor.js
@@ -1,72 +1,77 @@
 const express = require('express');
 const router = express.Router();
+//browser screen capture
 const capture = require('capture-chrome');
 const fs = require('fs');
+//model
 const Appstraction = require('../models/appstraction.js');
-//test variable 
+//test variable in absence of user authentification
 const currentUser = {username: 'Username', email: 'first.last@gmail.com', password: '12345'}
 
 //GET
-//Index
-router.get('/', (req,res) => {
-    res.render('index.ejs');
-});
-
-//Create
-router.get('/render', (req,res) => {
-    res.render('new.ejs');
-});
-
-//Iframe within Create
-router.get('/blank_canvas', (req,res) => {
-    //server identifies current user to attach their username to their content
-    res.render('blank_canvas.ejs',  {user: currentUser.username});
-});
-
-//Show + Edit 
-router.get('/gallery', (req,res) => {
-    Appstraction.find({user: currentUser.username}, (err, data) => {
-        res.render('show.ejs',{data: data, user: currentUser.username});
+    //Index
+    router.get('/', (req,res) => {
+        res.render('index.ejs');
     });
-});
 
-//Iframe within Show + Edit
-router.get('/saved_canvas/:id', (req,res) => {
-    Appstraction.findById(req.params.id, (err, doc) => {
-        res.render('saved_canvas.ejs',{doc: doc});
+    //New
+    router.get('/render', (req,res) => {
+        res.render('new.ejs');
     });
-}); 
 
-
-//POST: Save
-router.post('/blank_canvas', (req, res) => {
-    Appstraction.create(req.body, (err, doc) => {
-        res.status(204).send();
+    //Iframe within New
+    router.get('/blank', (req,res) => {
+        //username is attached to rendered content to distinguish ownership and retrieve within show + edit route
+        res.render('blank.ejs',  {user: currentUser.username});
     });
-});
 
-//POST: Download
-router.post('/saved_canvas/:id', (req, res) => {
-        capture({
-            url: `http://localhost:3000/appstractor/saved_canvas/${req.params.id}`,
-            width: 3600,
-            height: 2400,
-            }).then(screenshot => {
-            fs.writeFileSync(`${__dirname + '/../'}/public/saved/appstractorUsername.png`, screenshot);
-            console.log('image saved');
+    //Show + Edit 
+    router.get('/gallery', (req,res) => {
+        Appstraction.find({user: currentUser.username}, (err, data) => {
+            //data passed for length and id tracking within script; username used to located correct png for download
+            res.render('show.ejs',{data: data, user: currentUser.username});
         });
-        res.status(204).send();
-});
+    });
+
+    //Iframe within Show + Edit
+    router.get('/saved/:id', (req,res) => {
+        //data[req.params.index] could have been used, though ID is more concrete and provides uniformity since delete route requires ID
+        Appstraction.findById(req.params.id, (err, doc) => {
+            //doc passed to retrieve html content of saved appstraction
+            res.render('saved.ejs',{doc: doc});
+        });
+    }); 
+
+//POST: 
+    // Creates doc
+    router.post('/blank', (req, res) => {
+        Appstraction.create(req.body, (err, doc) => {
+            res.status(204).send();
+        });
+    });
+
+    // Creates png 
+    router.post('/saved/:id', (req, res) => {
+            capture({
+                url: `http://localhost:3000/appstractor/saved/${req.params.id}`,
+                width: 3600,
+                height: 2400,
+                }).then(screenshot => {
+                fs.writeFileSync(`${__dirname + '/../'}/public/saved/appstractorUsername.png`, screenshot);
+                console.log('image saved');
+            });
+            res.status(204).send();
+    });
 
 //PUT
-router.put('/saved_canvas/:id', (req,res) => {
+router.put('/saved/:id', (req,res) => {
     Appstraction.findByIdAndUpdate(req.params.id,{$set: {dom: req.body.dom}}, {new: true}, (err, doc) => {
         res.status(204).send();
     });
 });
 
 //Delete
-router.delete('/saved_canvas/:id', (req, res) => {
+router.delete('/saved/:id', (req, res) => {
     Appstraction.findByIdAndDelete(req.params.id, (err, doc) => {
         res.status(204).send();
     });
