484880454b42c3fcb7cb39048a3e98083cfae273
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2019-11-25 12:09:46 -0500

    test

--- appstractor/db/connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/' + 'appstraction';
-
-mongoose.connect(mongoURI);
-mongoose.Promise = Promise;
-
-module.exports = mongoose;
--- appstractor/db/heroku.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const mongoURI = 'mongodb://pavlos:B1u3m00n@ds249008.mlab.com:49008/heroku_0thh8tzc';
-
-mongoose.connect(mongoURI);
-mongoose.Promise = Promise;
-
-module.exports = mongoose;
