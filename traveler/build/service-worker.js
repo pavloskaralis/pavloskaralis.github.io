9b0b5344c45840255d14bd6cd9b296fa8bc7f78a
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-08-15 12:37:12 -0400

    test

--- traveler/build/service-worker.js
+++ traveler/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71cd7c372f0da7527c719ccca7d8b100.js"
+  "/traveler/precache-manifest.4aa359acaefe1ac370dcaf36272f6cec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/traveler/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
